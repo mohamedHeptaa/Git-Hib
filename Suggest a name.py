696e074028a61070a13f154a4dec8562d2eaa9ea
HeptaDev <shomohamed362@gmail.com>
2025-06-14 12:12:55 +0700

    Name Suggestions Project

---
+++ Suggest a name.py
@@ -0,0 +1,8 @@
+#اقترحات اسماء قنوات اليوتيوب للمحترفين 
+print("YouTube channel name suggestions for professionals")
+#ادخل  اسمك او اسم شهرتك
+name =input("Enter your name or nickname ? \n")
+#ما هو نشطات قناتك علي اليوتيوب
+topic =input("What are your YouTube channel activities ? \n")
+#الاسم المقترح هو 
+print("The proposed name is " +  ( topic + " with " + name ))
